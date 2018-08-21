037add42e39192b9e461b47e9168659be194ff05
Brian Gibson <bggibson@hotmail.com>
2018-08-21 13:46:24 -0400

    Adds Welcome Page

--- src/main/java/com/example/oauth2/Oauth2Application.java
+++ src/main/java/com/example/oauth2/Oauth2Application.java
@@ -3,10 +3,33 @@ package com.example.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class Oauth2Application {
+@RestController
+public class Oauth2Application extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Oauth2Application.class, args);
